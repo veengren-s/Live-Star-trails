7cf953541023072147bb925888cca9be90f47b3c
veengren-s <71776777+veengren-s@users.noreply.github.com>
2023-10-08 15:13:41 -0600

    added folder monitering, Now is a LIVE stacker

--- Live trails.py
+++ Live trails.py
@@ -1,7 +1,9 @@
 import os
 import cv2
+import time
 WIDTH = 0
 HEIGHT = 0
+prevStacked = False
 
 # Scans the selected folder and returns an array with all the files
 def scan(filePath):
@@ -15,7 +17,7 @@ def load(fileList, filePath):
         end = files[length-3:length]
 
         if end == "jpg":
-            image = cv2.imread(filePath + "/" + files)
+            image = cv2.imread(filePath + "\\" + files)
             imageArray.append(image)
 
     return imageArray
@@ -31,7 +33,7 @@ def lighten(images):
     for x in range(0,width):
         for y in range(0,height):
             max = 0
-            for i in range(0,len(images)-1):
+            for i in range(0,len(images)):
                 if imageGray[i][x][y] > imageGray[max][x][y]:
                     max = i
             result[x][y][0] = images[max][x][y][0]
@@ -39,13 +41,45 @@ def lighten(images):
             result[x][y][2] = images[max][x][y][2]
     return result
         
-            
-def stack(filePath):
+#removes already stacked files and any other unwanted stuff
+def cullFileList(fileList,read):
+    culledList = []
+    for img in fileList:
+        if img not in read:
+            culledList.append(img)
+    
+    if "temp.jpg" in fileList:
+        culledList.append("temp.jpg")
+    return culledList
+    
+#Runs the stacking,scanning and reading steps
+def stack(filePath, read):
     fileList = scan(filePath)
-    images = load(fileList,"test")
+    fileList = cullFileList(fileList, read)
+    images = load(fileList,filePath)
     results = lighten(images)
-    # cv2.imshow("results", results)
-    # cv2.waitKey(0)
-    cv2.imwrite("test/test.jpg", results)
-stack("test")
-print("done")
+    cv2.imwrite(filePath+ "\\temp.jpg", results)
+    return results
+    prevStacked = True
+
+#checks for new files
+def check(filePath, read):
+    currentFiles = scan(filePath)
+
+    #we have a new file
+    if currentFiles != read:
+        print("new files")
+        stack(filePath, read)
+        read = currentFiles
+    else:
+        print("No new files")
+    return read
+
+read = []
+i = 0
+while(i < 20):
+    print(i)
+    res = check("Live-Star-trails\\test", read)
+    read = res
+    time.sleep(5)
+    i += 1
